c2d58fa6328ba8db43c8f6175e84e6fb9ccab75a
Viggo2001 <viggoent@gmail.com>
2025-04-07 06:24:14 +0200

    inital commit

---
+++ init.lua
@@ -0,0 +1,49 @@
+require 'core.options'
+require 'core.keymaps'
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+-- Process on top installs plugin manager itself
+
+require('lazy').setup({
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    ---@module "neo-tree"
+    ---@type neotree.Config?
+    opts = {
+      -- fill any relevant options here
+    },
+  },
+  {
+      'shaunsingh/nord.nvim',
+      lazy = false,
+      priority = 1000,
+      config = function()
+          vim.g.nord_contrast = true
+          vim.g.nord_borders = false
+          vim.g.nord_disable_background = true
+          vim.g.nord_italic = false
+          vim.g.nord_uniform_diff_background = true
+          vim.g.nord_bold = false
+
+          require('nord').set()
+      end
+  },
+  require 'plugins.bufferline',
+  require 'plugins.lsp'
+})
